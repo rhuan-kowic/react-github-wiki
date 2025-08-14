2fc662f855f602809d63cf3e43d26c9ad7be3f1c
Rhuan Kowic <dev.rhuankowic@gmail.com>
2025-08-14 10:14:33 -0300

    feat: adicionando estilo global.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,11 @@
+import gitLogo from "../assets/logo.png"
 
+import { Container } from "./styles";
 function App() {
   return (
-    <div className="App">
-      Ola
-    </div>
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt="Logo GitHub"/>
+    </Container>
   );
 }
 
