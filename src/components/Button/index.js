1448d48e1b11494d3f13fdd250a3ed14e0c9bf5e
Rhuan Kowic <dev.rhuankowic@gmail.com>
2025-08-18 09:55:40 -0300

    feat: adicionando funcao de adicionar repositorio

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ButtonContainer from "./styles";
+
+export default function Button({onClick}) {
+  return (
+    <ButtonContainer onClick={onClick}>
+      Buscar
+    </ButtonContainer>
+  );
+}
--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { ItemContainer } from "./styles";
-export default function ItemRepo() {
+export default function ItemRepo({ repo }) {
   return (
     <ItemContainer>
-      <h3>Rhuan</h3>
-      <p>repo/rhuan</p>
-      <a href="#">Ver repositorio</a> <br/>
-      <a href="#" className="remover">
+      <h3>{repo.name}</h3>
+      <p>{repo.full_name}</p>
+      <a href={repo.html_url} rel="noopener" target="_blank" >
+        Ver repositorio
+      </a>{" "}
+      <br />
+      <a href="#" className="remover" rel="noopener">
         Remover
       </a>
       <hr />
